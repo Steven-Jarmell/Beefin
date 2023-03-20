e61720ad0feb6fc9a854f2b3c17b96acdf33e75f
jonriklan <56042403+jonriklan@users.noreply.github.com>
2023-03-20 13:25:25 -0400

    commit1

---
+++ microservices-parent/group-service/src/main/java/com/beefin/services/model/Group.java
@@ -0,0 +1,17 @@
+package com.beefin.services.model;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.cloud.firestore.annotation.ServerTimestamp;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Group{
+
+}
