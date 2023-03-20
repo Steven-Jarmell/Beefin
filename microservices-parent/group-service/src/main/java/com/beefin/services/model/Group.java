cc87241f9b0d66320d0f55ceb9ab16c9358ea8c9
jonriklan <56042403+jonriklan@users.noreply.github.com>
2023-03-20 19:24:44 -0400

    Model service controller iteration 1

--- microservices-parent/group-service/src/main/java/com/beefin/services/model/Group.java
+++ microservices-parent/group-service/src/main/java/com/beefin/services/model/Group.java
@@ -13,5 +13,51 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Group{
+    private String id;
+    private String name;
+    private String groupLeader;
+    private ArrayList<User> leaderBoard = new ArrayList<User>();
+    private ArrayList<User> usersInvolved = new ArrayList<User>();
 
+    @ServerTimestamp
+    private Date createdAt;
+    @ServerTimestamp
+    private Date updatedAt;
+
+    public Group (String id,
+                  String name,
+                  String groupLeader,
+                  ArrayList<User> leaderBoard,
+                  ArrayList<User>usersInvolved){
+        this.id = id;
+        this.name = name;
+        this.groupLeader = groupLeader;
+        this.leaderBoard = leaderBoard;
+        this.usersInvolved = usersInvolved
+    }
+    public ArrayList<User> getUsersInvolved(){
+        return this.usersInvolved;
+    }
+
+    public ArrayList<User> getLeaderboard() {
+        return this.leaderboard;
+    }
+    public ArrayList<User> getGroupLeader(){
+        return this.groupLeader;
+    }
+    public void setLeaderboard(ArrayList<User> leaderBoard){
+        this.leaderBoard = leaderBoard;
+    }
+    public void renameGroup(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setUsersInvolved(ArrayList<User> usersInvolved){
+        this.usersInvolved = usersInvolved;
+    }
+    private void setGroupLeader(User groupLeader){
+        this.groupLeader = groupLeader;
+    }
 }
