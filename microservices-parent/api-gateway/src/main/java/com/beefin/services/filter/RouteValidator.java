3923e315a46ac98a7ad634927b75d027c049549f
Steven-Jarmell <sjj27@pitt.edu>
2023-03-29 21:07:54 -0400

    Implement custom verification process for users

--- microservices-parent/api-gateway/src/main/java/com/beefin/services/filter/RouteValidator.java
+++ microservices-parent/api-gateway/src/main/java/com/beefin/services/filter/RouteValidator.java
@@ -11,6 +11,7 @@ public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
             "api/auth/register",
             "api/auth/authenticate",
+            "api/auth/verify",
             "eureka/web"
     );
 
