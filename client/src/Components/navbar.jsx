40352320991d9893a7e15a3410a3e07c11853cfa
patelpratham11 <ppatel011102@gmail.com>
2023-04-06 15:24:25 -0400

    added beefin' logo to navbar

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -1,13 +1,14 @@
 import '../Style/navbar.css'
+import logo from "../assets/Beefin.png";
 
 const NavBar = (props) => {
   return (
     <div className='navbar-container'>
       <div className="navbar-buttons-container">
+        <img src={logo} alt="" />
         <button className='left'>My Profile</button>
-        <button className='left'>My Groups</button>
-        <button className='left'>Create a Group</button>
-        <button className='left'>Join a Group</button>
+        <button className='left'>See Groups</button>
+        <button className='left'>Create/Join a Group</button>
         <button className='left'>Workouts</button>
         <button className='right'>Logout</button>
       </div>
