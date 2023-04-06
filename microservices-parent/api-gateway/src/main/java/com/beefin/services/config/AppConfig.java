46e57ce835107e77cb1fe68c40700118d010be4e
Steven-Jarmell <sjj27@pitt.edu>
2023-04-06 16:30:12 -0400

    Remove unused imports in api-gateway

--- microservices-parent/api-gateway/src/main/java/com/beefin/services/config/AppConfig.java
+++ microservices-parent/api-gateway/src/main/java/com/beefin/services/config/AppConfig.java
@@ -1,15 +1,11 @@
 package com.beefin.services.config;
 
-import com.google.common.net.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
