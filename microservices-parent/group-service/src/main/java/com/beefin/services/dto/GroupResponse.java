ca27d4c8b6871e213fb19d7af2124c8679bf1efa
Steven-Jarmell <sjj27@pitt.edu>
2023-04-07 12:19:54 -0400

    Merge Jon's group service into my branch

---
+++ microservices-parent/group-service/src/main/java/com/beefin/services/dto/GroupResponse.java
@@ -0,0 +1,22 @@
+package com.beefin.services.dto;
+
+import com.beefin.services.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupResponse {
+    private String id;
+    private String name;
+    private User groupLeader;
+    private ArrayList<User> leaderBoard = new ArrayList<User>();
+    private ArrayList<User> usersInvolved = new ArrayList<User>();
+}
