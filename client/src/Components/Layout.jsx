c31cd6134a0288978ccd0e53b26328669afbe453
Steven-Jarmell <sjj27@pitt.edu>
2023-04-17 11:46:17 -0400

    Fix layout CSS Fix new group page

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -1,14 +1,15 @@
 import NavBar from "./navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import "../CSS/Layout.css"
 
 const Layout = () => {
     return (
-        <>
+        <div className="main-layout-container">
             <NavBar />
             <Outlet />
             <Footer />
-        </>
+        </div>
     );
 };
 
